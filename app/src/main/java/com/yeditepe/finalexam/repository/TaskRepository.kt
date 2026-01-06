85cf60336c0b65fe65e60bd82118451b63bf3c91
Samet Laçin <93186378+Burlagramm@users.noreply.github.com>
2026-01-06 15:23:02 +0300

    TaskRepo
    Implemented fetchTasks method in TaskRepository to retrieve and convert tasks from API response.

--- app/src/main/java/com/yeditepe/finalexam/repository/TaskRepository.kt
+++ app/src/main/java/com/yeditepe/finalexam/repository/TaskRepository.kt
@@ -4,10 +4,26 @@ import com.yeditepe.finalexam.api.TaskApi
 import com.yeditepe.finalexam.model.Task
 
 class TaskRepository(private val api: TaskApi) {
+     fun fetchTasks(): List<Task> {
+      
+        val response = api.getTasks()
+        
+     
+        return response.map { dto ->
+            Task(id = dto.id, title = dto.title, isCompleted = dto.completed)
+        }
+    }
+}
 
-    suspend fun fetchTasks(): List<Task> {
-        // TODO 1: Call API
-        // TODO 2: Convert TaskDto list to Task list
-        return TODO("Provide the return value")
+class TaskViewModel(private val repository: TaskRepository) : ViewModel() {
+
+    private val _taskList = mutableStateOf<List<Task>>(emptyList())
+    val taskList: State<List<Task>> = _taskList
+
+    init {
+ 
+        viewModelScope.launch {
+            _taskList.value = repository.fetchTasks()
+        }
     }
 }
